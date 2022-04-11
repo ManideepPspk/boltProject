3675cd529cd4c824217508d9d6ae4a7ed49f0a69
MANIDEEP GODAVARTHY <manideepgodavarthy@gmail.com>
2022-04-11 12:31:10 +0530

    chart bg

--- src/components/Vehicles/Chart.js
+++ src/components/Vehicles/Chart.js
@@ -15,23 +15,11 @@ const Chart = (props) => {
             datasets: [
                 {
                     data: datarestored?.bookings?.Max?.datavalue,
-                    backgroundColor: [
-                        "rgba(255, 99, 132, 0.2)",
-                        "rgba(54, 162, 235, 0.2)",
-                        "rgba(255, 206, 86, 0.2)",
-                        "rgba(75, 192, 192, 0.2)",
-                        "rgba(153, 102, 255, 0.2)",
-                        "rgba(255, 159, 64, 0.2)"
-                    ],
+                    
                     borderColor: [
-                        "rgba(255, 99, 132, 1)",
-                        "rgba(54, 162, 235, 1)",
-                        "rgba(255, 206, 86, 1)",
-                        "rgba(75, 192, 192, 1)",
-                        "rgba(153, 102, 255, 1)",
-                        "rgba(255, 159, 64, 1)"
+                        "rgba(66, 66, 66, 1)"
                     ],
-                    borderWidth: 1
+                    borderWidth: 1.2
                 }
             ]
         },
@@ -44,6 +32,13 @@ const Chart = (props) => {
                         }
                     }
                 ]
+            },
+            elements:{
+                point:{
+                    borderWidth: 0,
+                    radius: 2.2,
+                    backgroundColor: 'rgba(66,66,66,1)'
+                }
             },
             maintainAspectRatio: false,
             legend: {
